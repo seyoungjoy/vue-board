1fab154299bab231a68ac531665f00b8baaea942
seyoungjoy <seyoungjoy>
2022-05-09 20:48:35 +0900

    Read and Create

---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+export default [
+    // DB를 만들지 않고 더미 데이터를 만들어 연습해볼 예정
+    // 게시판 완성 후 DB도 만들어보자
+    {
+        writer : '아이린',
+        title: '레드벨벳',
+        content:'1'
+    },
+    {
+        writer : '슬기',
+        title: '레드벨벳',
+        content:'2'
+    },
+    {
+        writer : '웬디',
+        title: '레드벨벳',
+        content:'3'
+    },
+
+]
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Read from '@/components/Read'
+import Create from '@/components/Create'
+import Detail from '@/components/Detail'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Read',
+    component: Read
+  },
+
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/detail/:contentId',
+    name: 'Detail',
+    component: Detail
+  },
 ]
 
 const router = new VueRouter({
